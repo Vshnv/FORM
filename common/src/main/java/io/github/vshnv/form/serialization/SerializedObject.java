68752c7b0c06e9a4c8f7f499e6962a842c839b81
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-02-01 02:43:43 +0530

    Serialization Documentation

--- common/src/main/java/io/github/vshnv/form/serialization/SerializedObject.java
+++ common/src/main/java/io/github/vshnv/form/serialization/SerializedObject.java
@@ -2,20 +2,36 @@ package io.github.vshnv.form.serialization;
 
 import java.util.Map;
 
+/**
+ * Represents the serialized form of an object
+ */
 public final class SerializedObject {
+    // Name of primary key field
     private final String primaryKey;
+    // Map of all serialized values in the object
     private final Map<String, Object> data;
 
+    /**
+     * @param primaryKey primary key from the given map of data
+     * @param data the serialized data from an object
+     * @apiNote data must contain the primaryKey as a key
+     */
     public SerializedObject(String primaryKey, Map<String, Object> data) {
         this.primaryKey = primaryKey;
         this.data = data;
     }
 
+
     public String getPrimaryKey() {
         return primaryKey;
     }
 
+    /**
+     * @return Map of serialized data. Key being field names, value being field value
+     * @apiNote key might not be ACTUAL field name but rather the field to be used in storage
+     */
     public Map<String, Object> getData() {
         return data;
     }
+
 }
--- common/src/main/java/io/github/vshnv/form/serialization/Serializer.java
+++ common/src/main/java/io/github/vshnv/form/serialization/Serializer.java
@@ -1,7 +1,11 @@
 package io.github.vshnv.form.serialization;
 
-import java.util.Map;
-
 public interface Serializer {
+    /**
+     * Creates a internal serialized representation of the object
+     * @param obj Object to be serialized
+     * @return Serialized version of the object with map of data and key
+     * @apiNote Object must have a field annotated with {@link io.github.vshnv.form.annotation.PrimaryKey}
+     */
     SerializedObject serialize(Object obj);
 }
